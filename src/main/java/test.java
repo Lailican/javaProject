d0d9f6694a670527f6dfad25375f621953174c9b
lailican <664840051@qq.com>
2020-04-03 15:14:24 +0800

    第一次提交

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,2 +1,23 @@
+import com.alibaba.fastjson.JSONObject;
+
+import java.lang.reflect.Field;
+
 public class test {
+    public static void main(String[] args) throws IllegalAccessException {
+        user a = new user();
+        a.setAddress("杭州滨江");
+        a.setId("dasdasdadgxcwertqw");
+        a.setName("来利灿");
+        JSONObject.toJSON(a);
+       String ad= JSONObject.toJSONString(a);
+        System.out.println(ad);
+        Class<user> userClass = user.class;
+        Field[] declaredFields = userClass.getDeclaredFields();
+        for (int i = 0; i < declaredFields.length; i++) {
+            Field declaredField = declaredFields[i];
+            System.out.println(declaredField.getName());
+            System.out.println(declaredField.get(a));
+        }
+
+    }
 }
