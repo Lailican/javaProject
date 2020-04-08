67cde92351032948b137d5ec0997953ae28afe5a
lailican <664840051@qq.com>
2020-04-08 15:45:57 +0800

    提交master分支

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -8,20 +8,15 @@ public class test {
         a.setAddress("杭州滨江");
         a.setId("dasdasdadgxcwertqw");
         a.setName("来利灿");
-        //dev改动 哈哈哈
         JSONObject.toJSON(a);
        String ad= JSONObject.toJSONString(a);
         System.out.println(ad);
-        //master改动
         Class<user> userClass = user.class;
         Field[] declaredFields = userClass.getDeclaredFields();
         for (int i = 0; i < declaredFields.length; i++) {
             Field declaredField = declaredFields[i];
             System.out.println(declaredField.getName());
-            //dev改动 哈哈哈5
             System.out.println(declaredField.get(a));
         }
-        //dev改动 哈哈哈2
-
     }
 }
